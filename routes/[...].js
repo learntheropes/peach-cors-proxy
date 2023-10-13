76ccd7a9b7d8de38c2980ba421109430ac78ab84
learntheropes <giovanni@learntheropes.xyz>
2023-10-13 18:23:04 +0200

    first commit

---
+++ routes/[...].js
@@ -0,0 +1,37 @@
+import { readBody, getQuery, getRequestHeaders } from 'h3';
+import { ofetch } from 'ofetch';
+
+export default defineEventHandler(async (event) => {
+
+  const apiFetch = ofetch.create({
+    baseURL: `https://api.peachbitcoin.com`,
+    async onRequestError({ request, error }) {
+      console.log('[fetch request error]', request, error);
+    },
+    async onResponseError({ request, response }) {
+      console.log('[fetch response error]', request, response.status, response.body);
+    }
+  });
+
+  const method = event.method;
+
+  const params = event.context.params._;
+
+  const headers = getRequestHeaders(event);
+  headers.host = 'api.peachbitcoin.com'
+
+  const query = getQuery(event);
+
+  let body;
+  if (method !== 'GET') {
+
+    body = await readBody(event);
+  };
+
+  return await apiFetch(`/${params}`, {
+    method,
+    headers,
+    query,
+    body
+  });
+});
